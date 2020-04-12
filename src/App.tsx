b5fef803125815ea9bd7c4bfea24a8da1e73b745
陈烨 <chenye.meskil@bytedance.com>
2020-04-12 16:22:39 +0800

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,29 @@
-import React, {useCallback, useState} from 'react'
-import {library} from "@fortawesome/fontawesome-svg-core"
-import {fas} from "@fortawesome/free-solid-svg-icons"
-import './App.css'
-import Menu from "./components/menu/Menu"
-import MenuItem from "./components/menu/MenuItem"
-import SubMenu from "./components/menu/SubMenu";
-import Transition from "./components/transition/Transition";
-import Button from './components/button/Button'
-
-library.add(fas)
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 interface IAppPropsType {
 
 }
 
 const App: React.FC<IAppPropsType> = () => {
-  const [show, setShow] = useState(false)
-  const handleSelect = useCallback((index: string): void => {
-    console.log(index)
-  }, [])
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (files) {
+      const uploadedFile = files[0]
+      const formData = new FormData()
+      formData.append(uploadedFile.name, uploadedFile)
+      axios.post('https://jsonplaceholder.typicode.com/posts', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(res => {
+        console.log(res)
+      })
+    }
+  }
   return (
-    <div className="App">
-      <Menu defaultIndex='0' onSelect={handleSelect} defaultOpenSubMenus={['2']}>
-        <MenuItem>cool link</MenuItem>
-        <MenuItem disabled>cool link2</MenuItem>
-        <SubMenu title="dropdown">
-          <MenuItem>dropdown 1</MenuItem>
-          <MenuItem>dropdown 2</MenuItem>
-        </SubMenu>
-        <MenuItem>cool link3</MenuItem>
-      </Menu>
-      <Button size='lg' onClick={() => {
-        setShow(!show)
-      }}>Toggle</Button>
-      <Transition in={show} timeout={300} animation="zoom-in-left">
-        <div>
-          <p>
-            Edit <code>src/App.tsc</code> and save to reload.
-          </p>
-          <p>
-            Edit <code>src/App.tsc</code> and save to reload.
-          </p>
-          <p>
-            Edit <code>src/App.tsc</code> and save to reload.
-          </p>
-          <p>
-            Edit <code>src/App.tsc</code> and save to reload.
-          </p>
-          <p>
-            Edit <code>src/App.tsc</code> and save to reload.
-          </p>
-        </div>
-      </Transition>
-      <Transition
-        in={show}
-        timeout={300}
-        animation='zoom-in-left'
-        wrapper
-      >
-        <Button btnType='primary' size='lg'>A Large Button</Button>
-      </Transition>
+    <div className='App' style={{marginTop: '100px', marginLeft: '100px'}}>
+      <input type="file" name='myFile' onChange={handleFileChange}/>
     </div>
   )
 }
